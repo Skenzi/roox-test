13ef72941280ab9784115184f1e7a3f248b14d43
Skenzi <fitronavm@gmail.com>
2022-04-20 21:35:34 +0500

    add element in Users; update styles for form

--- src/components/ProfileForm.tsx
+++ src/components/ProfileForm.tsx
@@ -36,41 +36,41 @@ function ProfileForm({
   return (
     <form onSubmit={onSubmit}>
       <div className="form-box">
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="name" className="form-label">Name</label>
           <input id="name" type="text" className={`${classFormElement} ${errors.name ? 'form-element--error' : ''}`} value={formData.name} readOnly={!isEdit} onChange={handlerFormData('name')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="username" className="form-label">Username</label>
           <input id="username" type="text" className={`${classFormElement} ${errors.username ? 'form-element--error' : ''}`} value={formData.username} readOnly={!isEdit} onChange={handlerFormData('username')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="email" className="form-label">Email</label>
           <input id="email" type="text" className={`${classFormElement} ${errors.email ? 'form-element--error' : ''}`} value={formData.email} readOnly={!isEdit} onChange={handlerFormData('email')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="street" className="form-label">Street</label>
           <input id="street" type="text" className={`${classFormElement} ${errors.street ? 'form-element--error' : ''}`} value={formData.street} readOnly={!isEdit} onChange={handlerFormData('street')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="city" className="form-label">City</label>
           <input id="city" type="text" className={`${classFormElement} ${errors.city ? 'form-element--error' : ''}`} value={formData.city} readOnly={!isEdit} onChange={handlerFormData('city')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="zipCode" className="form-label">Zip code</label>
           <input id="zipCode" type="text" className={`${classFormElement} ${errors.zipCode ? 'form-element--error' : ''}`} value={formData.zipCode} readOnly={!isEdit} onChange={handlerFormData('zipCode')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="phone" className="form-label">Phone</label>
           <input id="phone" type="text" className={`${classFormElement} ${errors.phone ? 'form-element--error' : ''}`} value={formData.phone} readOnly={!isEdit} onChange={handlerFormData('phone')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="website" className="form-label">Website</label>
           <input id="website" type="text" className={`${classFormElement} ${errors.website ? 'form-element--error' : ''}`} value={formData.website} readOnly={!isEdit} onChange={handlerFormData('website')} />
         </div>
-        <div className="form-group f-wrap">
+        <div className="form-group">
           <label htmlFor="comment" className="form-label">Comment</label>
-          <textarea id="comment" className={classFormElement} value={formData.comment} readOnly={!isEdit} onChange={handlerFormData('comment')} />
+          <textarea id="comment" className={`${classFormElement} form-element-textarea`} value={formData.comment} readOnly={!isEdit} onChange={handlerFormData('comment')} />
         </div>
       </div>
       <button className={`button float-r ${isEdit ? 'button--active' : 'button--inactive'}`} type="submit">Отправить</button>
--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -4,32 +4,37 @@ import { UsersProps } from '../types/types';
 
 function Users({ users, setActiveUser }: UsersProps) {
   return (
-    <ul className="list-users">
-      {users.map((user) => (
-        <li key={user.id} className="list-users__item">
-          <div className="item__info">
-            <div className="info__item">
-              <span className="item__name">ФИО:</span>
-              {' '}
-              <span className="item__content">{user.name}</span>
+    <>
+      <ul className="list-users">
+        {users.map((user) => (
+          <li key={user.id} className="list-users__item">
+            <div className="item__info">
+              <div className="info__item">
+                <span className="item__name">ФИО:</span>
+                {' '}
+                <span className="item__content">{user.name}</span>
+              </div>
+              <div className="info__item">
+                <span className="item__name">Город:</span>
+                {' '}
+                <span className="item__content">{user.address.city}</span>
+              </div>
+              <div className="info__item">
+                <span className="item__name">Компания:</span>
+                {' '}
+                <span className="item__content">{user.company.name}</span>
+              </div>
             </div>
-            <div className="info__item">
-              <span className="item__name">Город:</span>
-              {' '}
-              <span className="item__content">{user.address.city}</span>
-            </div>
-            <div className="info__item">
-              <span className="item__name">Компания:</span>
-              {' '}
-              <span className="item__content">{user.company.name}</span>
-            </div>
-          </div>
-          <Link to="/profile" state={{ user }}>
-            <button type="button" className="button button--link" onClick={() => setActiveUser(user)}>Подробнее</button>
-          </Link>
-        </li>
-      ))}
-    </ul>
+            <Link to="/profile" state={{ user }}>
+              <button type="button" className="button button--link" onClick={() => setActiveUser(user)}>Подробнее</button>
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="float-r">
+        {`Найдено ${users.length} пользователей`}
+      </div>
+    </>
   );
 }
 
